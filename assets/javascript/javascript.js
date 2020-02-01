07b9455d02d204986373f8014b6341ecca867025
Logan Moss <lrdjmoss@gmail.com>
2020-01-31 19:14:46 -0500

    added working fonts

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -4,11 +4,9 @@ $(function() {
         selectedClass = $(this).attr("data-rel");
         $("#gallery").fadeTo(100, 0.1);
         $("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
-        setTimeout(function() {
+        setTimeout(function(){
             $("."+selectedClass).fadeIn().addClass('animation');
             $("#gallery").fadeTo(300, 1);
         }, 300);
     });
- });
-
- 
+});
