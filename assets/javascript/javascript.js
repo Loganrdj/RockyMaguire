d12b463d42c0b8ae152c9261b3c9fc80e2e5ac6c
Logan Moss <lrdjmoss@gmail.com>
2020-02-04 13:10:18 -0500

    minor bug fixes

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -26,14 +26,12 @@ $(document).ready(function(){
         $(this).text("Submit");
     })
 
-    $(".animatedImg").mouseenter(function()
-    { 
+    $(".animatedImg").mouseenter(function(){ 
         $(this).toggleClass('classWithShadow');
         $(this).animate({ top: "-=2px" }, {duration: 200, queue: false});
         $(this).animate({ left: "-=2px" }, {duration: 200, queue: false});
     });
-    $(".animatedImg").mouseleave(function()
-    { 
+    $(".animatedImg").mouseleave(function(){ 
         $(this).toggleClass('classWithShadow');
         $(this).animate({ top: "+=2px" }, {duration: 200, queue: false});
         $(this).animate({ left: "+=2px" }, {duration: 200, queue: false});
@@ -41,36 +39,46 @@ $(document).ready(function(){
 
     // Testing
 
-    $(document).ready(function () {
-        var carousel = $("#carousel").waterwheelCarousel({
+
+    var carousel = $("#carousel").waterwheelCarousel({
         flankingItems: 3,
-        });
+    });
+
+    // Right: 39
+    // Left: 37
+
     
-        $('#prev').bind('click', function () {
+
+    $('#prev').bind('click', function (event) {
+        event.preventDefault();
         carousel.prev();
         return false
-        });
+    });
+
     
-        $('#next').bind('click', function () {
+
+
+    $('#next').bind('click', function (event) {
+        event.preventDefault();
         carousel.next();
         return false;
-        });
     });
+
 
     $("#carousel").waterwheelCarousel({
         // number tweeks to change apperance
         startingItem:               0,   // item to place in the center of the carousel. Set to 0 for auto
-        separation:                 175, // distance between items in carousel
-        separationMultiplier:       0.6, // multipled by separation distance to increase/decrease distance for each additional item
-        horizonOffset:              0,   // offset each item from the "horizon" by this amount (causes arching)
+        separation:                 175, // 175 distance between items in carousel
+        separationMultiplier:       .85, // .85 multipled by separation distance to increase/decrease distance for each additional item
+        horizonOffset:              25,   // offset each item from the "horizon" by this amount (causes arching)
         horizonOffsetMultiplier:    1,   // multipled by horizon offset to increase/decrease offset for each additional item
         sizeMultiplier:             0.7, // determines how drastically the size of each item changes
         opacityMultiplier:          0.8, // determines how drastically the opacity of each item changes
         horizon:                    0,   // how "far in" the horizontal/vertical horizon should be set from the container wall. 0 for auto
-        flankingItems:              2,   // the number of items visible on either side of the center                  
+        flankingItems:              4,   // the number of items visible on either side of the center                  
     
         // animation
-        speed:                      500,      // speed in milliseconds it will take to rotate from one to the next
+        speed:                      350,      // speed in milliseconds it will take to rotate from one to the next
         animationEasing:            'linear', // the easing effect to use when animating
         quickerForFurther:          true,     // set to true to make animations faster when clicking an item that is far away from the center
         edgeFadeEnabled:            false,    // when true, items fade off into nothingness when reaching the edge. false to have them move behind the center image
@@ -80,14 +88,14 @@ $(document).ready(function(){
         autoPlay:                   0,                 // indicate the speed in milliseconds to wait before autorotating. 0 to turn off. Can be negative
         orientation:                'horizontal',      // indicate if the carousel should be 'horizontal' or 'vertical'
         activeClassName:            'carousel-center', // the name of the class given to the current item in the center
-        keyboardNav:                false,             // set to true to move the carousel with the arrow keys
+        keyboardNav:                true,             // set to true to move the carousel with the arrow keys
         keyboardNavOverride:        true,              // set to true to override the normal functionality of the arrow keys (prevents scrolling)
         imageNav:                   true,              // clicking a non-center image will rotate that image to the center
     
         // preloader
         preloadImages:              true,  // disable/enable the image preloader. 
-        forcedImageWidth:           0,     // specify width of all images; otherwise the carousel tries to calculate it
-        forcedImageHeight:          0,     // specify height of all images; otherwise the carousel tries to calculate it
+        forcedImageWidth:           800,     // specify width of all images; otherwise the carousel tries to calculate it
+        forcedImageHeight:          600,     // specify height of all images; otherwise the carousel tries to calculate it
     
         // callback functions
         movingToCenter:             $.noop, // fired when an item is about to move to the center position
